6e5df101f5190191c4c4237e1a4b193549793c3c
Manish V <imvmanish19@gmail.com>
2020-06-15 15:51:21 +0530

    create model added

--- createmodel.js
+++ createmodel.js
@@ -1,1 +1,29 @@
+const dotenv = require('dotenv').config();
 const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+//console.log(DataTypes)
+
+const sequelize = new Sequelize('testdb2','alien',process.env.password,{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+const Student = sequelize.define('student', {
+    name: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    age: {
+        type:DataTypes.INTEGER(2),
+        allowNull: false,
+        defaultValue: -1
+    },
+    place: {
+        type: DataTypes.STRING(20)
+    }
+});
+
+sequelize.sync({alter: true})
+.then(() => console.log('Database Synchronized'))
+.catch(console.error)
